2960ef10ab6bc839663741facaa90190dce880c9
Narendra Ram <email@narenkram.com>
2024-12-27 22:50:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5178, // 5178 is the root cause of
+    // WARNING: Port 5178 causes BAD_REQUEST_ERROR during entity sync
+    // Error: {"error":{"code":"BAD_REQUEST_ERROR","description":"given field is an invalid field","source":"NA","step":"entity_sync","reason":"only valid field of entities are accepted"}}
+    // if port is changed to the vite default 5173, it will work with no issues.
+    port: 5178,
     proxy: {
       '/subscriptions': {
         target: `${process.env.VITE_API_URL}/subscriptions`,
@@ -19,5 +22,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()]
+  plugins: [vue()],
 })
